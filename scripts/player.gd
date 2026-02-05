57e14a72d5e553478d2c9d85ab50fdd7546045d4
Stefan Falkenstein <stefan@falkenste.in>
2026-02-05 03:28:48 -0500

    center booster

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,9 +6,11 @@ signal update_fuel_percentage(percentage: float)
 @export var debug := false
 var _force_active := true
 var _left_force_vector := Vector2.ZERO
+var _center_force_vector := Vector2.ZERO
 var _right_force_vector := Vector2.ZERO
 var _remaining_fuel_ratio := START_FUEL_RATIO
 var _left_booster: Sprite2D = null
+var _center_booster: Sprite2D = null
 var _right_booster: Sprite2D = null
 
 const MAX_FORCE_MAGNITUDE := 100.0
@@ -18,32 +20,38 @@ const FUEL_BURNED_PER_FORCE_SECOND := 0.0008
 func _physics_process(_delta: float) -> void:
 	if _force_active:
 		apply_force(_left_force_vector, transform.basis_xform(_left_booster.position))
+		apply_force(_center_force_vector, transform.basis_xform(_center_booster.position))
 		apply_force(_right_force_vector, transform.basis_xform(_right_booster.position))
 	
 	
 func _process(delta: float) -> void:
 	var left_boost_pressed := Input.is_action_pressed("left_boost")
+	var center_boost_pressed := Input.is_action_pressed("center_boost")
 	var right_boost_pressed := Input.is_action_pressed("right_boost")
 	var has_fuel := _remaining_fuel_ratio > 0.0
 	
-	_force_active = left_boost_pressed or right_boost_pressed
+	_force_active = left_boost_pressed or right_boost_pressed or center_boost_pressed
 	
 	_left_force_vector = Vector2.ZERO
+	_center_force_vector = Vector2.ZERO
 	_right_force_vector = Vector2.ZERO
 	_left_booster.visible = left_boost_pressed and has_fuel
+	_center_booster.visible = center_boost_pressed and has_fuel
 	_right_booster.visible = right_boost_pressed and has_fuel
 	
 	if has_fuel:
 		var force_dir := transform.basis_xform(Vector2.RIGHT)
 		if left_boost_pressed:
 			_left_force_vector = MAX_FORCE_MAGNITUDE * force_dir
+		if center_boost_pressed:
+			_center_force_vector = MAX_FORCE_MAGNITUDE * force_dir
 		if right_boost_pressed:
 			_right_force_vector = MAX_FORCE_MAGNITUDE * force_dir
 	
 	_remaining_fuel_ratio = clampf(
 		_remaining_fuel_ratio 
 			- FUEL_BURNED_PER_FORCE_SECOND 
-			* (_left_force_vector + _right_force_vector).length()
+			* (_left_force_vector + _right_force_vector + _center_force_vector).length()
 			* delta,
 		0, 1.0)
 	
@@ -55,9 +63,11 @@ func _process(delta: float) -> void:
 func _enter_tree() -> void:
 	_left_booster = $LeftBooster
 	_right_booster = $RightBooster
+	_center_booster = $CenterBooster
 	
 	_left_booster.visible = false
 	_right_booster.visible = false
+	_center_booster.visible = false
 
 
 
