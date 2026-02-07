bbdb0ce8b5a0f84c2fdc67aa6115df16b9d631ae
Stefan Falkenstein <stefan@falkenste.in>
2026-02-06 20:11:35 -0500

    gravity scale

--- scripts/gravity_body.gd
+++ scripts/gravity_body.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 const DENSITY := 1.0
+const GRAVITY_SCALE := 10.0
 
-
-## acceleration due to gravity at the given position
+## Acceleration due to gravity at the given position
 func gravity_at(pos: Vector2) -> Vector2:
-    var radius := (pos - global_position).length()
-    assert(radius >= scale.x)
-    var mass := PI * scale.x * scale.x * DENSITY
-    return mass * 10.0 / (radius * radius)
+	var radius := (pos - global_position).length()
+	var dir := (global_position - pos).normalized()
+	assert(radius >= scale.x)
+	var mass := PI * scale.x * scale.x * DENSITY
+	return mass * GRAVITY_SCALE / (radius * radius) * dir
